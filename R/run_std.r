e67b6c0bca2900b6af7cd932acb20a9f54b4e5e8
Miguel <rdxmig002@myuct.ac.za>
2021-07-14 11:02:13 +0200

    Add devtools and r-version text fn to run_std

--- R/run_std.r
+++ R/run_std.r
@@ -1,27 +1,60 @@
 #' @title Standard tasks for inside .Rprofile
 #'
 #' @export
-run_std <- function() {
+run_std <- function(env = rlang::caller_env()) {
   add_options_vsc()
 
   # open directories
   assign(
     "od",
     value = od,
-    envir = rlang::caller_env()
+    envir = env
+  )
+
+  # open directories
+  assign(
+    "rv",
+    value = rv,
+    envir = env
   )
 
   # open directories
   assign(
     "ob",
     value = ob,
-    envir = rlang::caller_env()
+    envir = env
   )
 
   # open directories
   assign(
     "kb",
     value = kb,
-    envir = rlang::caller_env()
+    envir = env
+  )
+
+  # add devtools::document
+  assign(
+    "dd",
+    value = dd,
+    envir = env
+  )
+  # open directories
+  assign(
+    "dt",
+    value = dt,
+    envir = env
+  )
+  # open directories
+  assign(
+    "dl",
+    value = dl,
+    envir = env
+  )
+
+  # open directories
+  assign(
+    "dbi",
+    value = dbi,
+    envir = env
   )
 }

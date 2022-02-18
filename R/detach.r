61713398de99b4ad72a2842ede527e68a40acd63
Miguel Rodo <rdxmig002@myuct.ac.za>
2022-02-18 13:47:48 +0200

    Add convenience fn to detach pkgs

---
+++ R/detach.r
@@ -0,0 +1,25 @@
+#' @title Detach a package
+#'
+#' @description
+#' Wrapper function around library and detach
+#' to detach packages.
+#'
+#' @param x character vector.
+#' Names of packages to detach.
+#'
+#' @export
+.detach_pkg <- function(x) {
+  for (xx in x) {
+    library(xx, character.only = TRUE)
+    args_list <- list(
+          name = paste0("package:", xx),
+          unload = TRUE,
+          character.only = TRUE
+        )
+    do.call(
+        what = "detach",
+        args = args_list
+    )
+  }
+  invisible(TRUE)
+}
